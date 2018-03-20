9f78a3965e2e12b6990df26fd3cc7e9fe1c2c973
nico24687 <theonlynico@hotmail.com>
2018-03-19 18:21:23 -0600

    created seeds for foods

--- knexfile.js
+++ knexfile.js
@@ -13,6 +13,19 @@ module.exports = {
     useNullAsDefault: true
   },
 
+  test: {
+    client: 'pg',
+    connection: 'postgres://localhost/quantified_self_test',
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/test'
+    },
+    useNullAsDefault: true
+  },
+
+
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL,
