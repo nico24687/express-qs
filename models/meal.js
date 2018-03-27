a053da649cb7957f2f8964428590e65a1a307917
seoulstice <jungyoungjoon@gmail.com>
2018-03-27 12:36:29 -0600

    Created find method

--- models/meal.js
+++ models/meal.js
@@ -3,11 +3,22 @@ const configuration = require('../knexfile')[environment]
 const database = require('knex')(configuration)
 
 class Meal {
-  findAll(){
+  static findAll(){
     return database.raw(
       'SELECT m.*, json_agg(f.*) AS foods FROM meals m INNER JOIN meal_foods mf ON m.id = mf.meal_id INNER JOIN foods f ON mf.food_id = f.id GROUP BY m.id'
     ).then(meals => {
-      res.json(meals.rows)
+      return meals.rows
+    })
+  }
+
+  static find(id) {
+    return database.raw(
+      'SELECT m.*, json_agg(f.*) AS foods FROM meals m INNER JOIN meal_foods mf ON m.id = mf.meal_id INNER JOIN foods f ON mf.food_id = f.id where m.id = ? GROUP BY m.id',
+      [id]
+    ).then(meal => {
+      return meal.rows[0]
     })
   }
 }
+
+module.exports = Meal
