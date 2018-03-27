3ab98d14020c2df3f90aa6c596cd9b092346dbfe
seoulstice <jungyoungjoon@gmail.com>
2018-03-27 12:03:35 -0600

    Created meal model

---
+++ models/meal.js
@@ -0,0 +1,13 @@
+const environment = process.env.NODE_ENV || 'development'
+const configuration = require('../knexfile')[environment]
+const database = require('knex')(configuration)
+
+class Meal {
+  findAll(){
+    return database.raw(
+      'SELECT m.*, json_agg(f.*) AS foods FROM meals m INNER JOIN meal_foods mf ON m.id = mf.meal_id INNER JOIN foods f ON mf.food_id = f.id GROUP BY m.id'
+    ).then(meals => {
+      res.json(meals.rows)
+    })
+  }
+}
