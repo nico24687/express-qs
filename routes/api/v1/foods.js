6398d351ddd60611277a9a58416b37a5dd1e3943
seoulstice <jungyoungjoon@gmail.com>
2018-03-27 12:37:28 -0600

    refactored routes

--- routes/api/v1/foods.js
+++ routes/api/v1/foods.js
@@ -3,10 +3,7 @@ const router = express.Router()
 const environment = process.env.NODE_ENV || 'development'
 const configuration = require('../../../knexfile')[environment]
 const database = require('knex')(configuration)
-const Food = require('../../../models/food')
 const foodsController = require('../../../controllers/foodsController')
-
-
 
 router.get('/', foodsController.index)
 
@@ -14,10 +11,8 @@ router.get('/:id', foodsController.show)
 
 router.post('/', foodsController.create )
 
-
 router.put('/:id', foodsController.update)
 
 router.delete('/:id', foodsController.destroy)
 
 module.exports = router
-
