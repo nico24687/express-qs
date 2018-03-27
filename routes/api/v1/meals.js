6a6cca7ffd4133fbbf81706cf845da1f1e7b78f6
seoulstice <jungyoungjoon@gmail.com>
2018-03-27 12:02:33 -0600

    Refactored meals index route

--- routes/api/v1/meals.js
+++ routes/api/v1/meals.js
@@ -3,13 +3,26 @@ const router = express.Router()
 const environment = process.env.NODE_ENV || 'development'
 const configuration = require('../../../knexfile')[environment]
 const database = require('knex')(configuration)
+const mealsController = require('../../../controllers/mealsController')
 
-router.get('/', (req,res,next) => {
+// router.get('/', (req,res,next) => {
+//   database.raw(
+//     'SELECT m.*, json_agg(f.*) AS foods FROM meals m INNER JOIN meal_foods mf ON m.id = mf.meal_id INNER JOIN foods f ON mf.food_id = f.id GROUP BY m.id'
+//   ).then(meals => {
+//     res.json(meals.rows)
+//   })
+// })
+
+router.get('/', mealsController.index)
+
+router.get('/:meal_id/foods' (req, res, next) => {
   database.raw(
-    'SELECT m.*, json_agg(f.*) AS foods FROM meals m INNER JOIN meal_foods mf ON m.id = mf.meal_id INNER JOIN foods f ON mf.food_id = f.id GROUP BY m.id'
-  ).then(meals => {
-    res.json(meals.rows)
+    'SELECT m.*, json_agg(f.*) AS foods FROM meals m INNER JOIN meal_foods mf ON m.id = mf.meal_id INNER JOIN foods f ON mf.food_id = f.id where m.id = ? GROUP BY m.id',
+      [meal_id]
+  ).then(meal => {
+    res.json(meal.rows[0])
   })
-})
+}
+
 
 module.exports = router
