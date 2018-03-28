a6c5d1a6ba59c2101ef1d374c934e0efcde0c3f3
seoulstice <jungyoungjoon@gmail.com>
2018-03-28 13:48:50 -0600

    Added to CORs headers

--- app.js
+++ app.js
@@ -56,11 +56,11 @@ app.use(function(err, req, res, next) {
 });
 
 app.use(function(req, res, next) {
-  // res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "https://nico24687.github.io/quantified-self");
   res.header('Access-Control-Allow-Methods', 'POST, PATCH, GET, DELETE');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
-});
+})
 
 
 module.exports = app;
