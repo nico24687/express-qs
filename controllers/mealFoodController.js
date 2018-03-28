281058bd5575e31324b32e7e480c546a446b4547
seoulstice <jungyoungjoon@gmail.com>
2018-03-28 12:54:21 -0600

    Created destory method

--- controllers/mealFoodController.js
+++ controllers/mealFoodController.js
@@ -10,14 +10,34 @@ class MealFoodController {
       .then(mealFood => {
         if(!mealFood) {
           return res.sendStatus(404)
-        } else {
+        }
+        else {
           return res.status(201).send({message: `Added ${mealFood.food_name} to ${mealFood.meal_name}`})
-          // return res.json(mealFood)
-      }
+        }
     })
   }
 
+  static destroy(req, res, next) {
+    let mealId = req.params.mealId
+    let foodId = req.params.foodId
 
+    let mealFood = MealFood.find(mealId, foodId)
+
+    Promise.all([mealFood])
+      .then(mealFood => {
+        if (!mealFood[0]) {
+          return res.sendStatus(404)
+        }
+        else {
+          MealFood.delete(mealId, foodId)
+          .then(response => {
+            res.status(200).send({
+              message: `Removed ${response[1].name} from ${response[0].name}`
+          })
+        })
+      }
+    })
+  }
 }
 
 module.exports = MealFoodController
