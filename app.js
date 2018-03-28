f92122279acf0418516755499b06e19873c88d23
seoulstice <jungyoungjoon@gmail.com>
2018-03-28 12:53:18 -0600

    Added CORs protection

--- app.js
+++ app.js
@@ -55,4 +55,12 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'POST, GET, DELETE');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
 module.exports = app;
