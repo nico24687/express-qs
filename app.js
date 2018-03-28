52bf140bfde910c3eef8c68bf772d535a718ebbc
nico24687 <theonlynico@hotmail.com>
2018-03-28 17:43:03 -0600

    moved cors up in file structure

--- app.js
+++ app.js
@@ -33,6 +33,13 @@ app.use(sassMiddleware({
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  next();
+})
+
 app.use('/', index);
 app.use('/users', users);
 app.use('/api/v1/foods', apiFoods)
@@ -59,12 +66,7 @@ app.use(function(err, req, res, next) {
 // app.options('*', cors())
 // app.use(cors({origin: ''}))
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Methods', "*");
-  res.header("Access-Control-Allow-Headers", "*");
-  next();
-})
+
 
 
 module.exports = app;
