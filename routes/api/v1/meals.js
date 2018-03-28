219fe072612c395375e7940bbc3b40aff9779f0a
seoulstice <jungyoungjoon@gmail.com>
2018-03-28 10:19:12 -0600

    Added require mealFoodController

--- routes/api/v1/meals.js
+++ routes/api/v1/meals.js
@@ -4,11 +4,15 @@ const environment = process.env.NODE_ENV || 'development'
 const configuration = require('../../../knexfile')[environment]
 const database = require('knex')(configuration)
 const mealsController = require('../../../controllers/mealsController')
+const mealFoodController = require('../../../controllers/mealFoodController')
 
 router.get('/', mealsController.index)
 
 router.get('/:id/foods', mealsController.show)
 
-router.post('/:id/foods', mealsController.post)
+router.post('/:id/foods', mealFoodController.create)
+
+router.delete('/:id/foods/:id', mealFoodController.destroy)
+
 
 module.exports = router
