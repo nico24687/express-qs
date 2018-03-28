3226ac08a0034be37531f2c502c02274c91df47d
nico24687 <theonlynico@hotmail.com>
2018-03-28 17:57:04 -0600

    added a patch route

--- routes/api/v1/foods.js
+++ routes/api/v1/foods.js
@@ -13,6 +13,8 @@ router.post('/', foodsController.create )
 
 router.put('/:id', foodsController.update)
 
+router.patch('/:id', foodsController.update)
+
 router.delete('/:id', foodsController.destroy)
 
 module.exports = router
